c5285098d3bb279b1a3a576c51c1f74ced31340c
Krakoer <axelrousselot@outlook.fr>
2022-11-01 19:14:28 +0100

    Correct job store

--- front/src/components/AgentDetails.vue
+++ front/src/components/AgentDetails.vue
@@ -1,17 +1,17 @@
 <template>
     <h4>Jobs of {{agent.id}}</h4>
-    <JobsTable :jobs="agentJobs"></JobsTable>
+    <JobsTable :jobs="agentJobs(agent.id)"></JobsTable>
 </template>
 
 <script setup>
 import { useJobsStore } from '@/stores/jobs';
 import JobsTable from "@/components/JobsTable.vue"
+
 const props = defineProps({
     agent: Object
 })
 
 const jobStore = useJobsStore();
 
-const agentJobs = jobStore.getByAgent(props.agent.id)
-
+const agentJobs = jobStore.getByAgent
 </script>
--- front/src/components/AgentRow.vue
+++ front/src/components/AgentRow.vue
@@ -26,10 +26,10 @@ td{
 }
 tr:hover
 {
-    background-color: #d4d4d4;
+    background-color: #e9e9e9;
 }
 tr.selected
 {
-    background-color: #d4d4d4;
+    background-color: #e9e9e9;
 }
 </style>
